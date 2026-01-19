44e5c486b66df79e53b06abc738cbb86900d5a65
jonnarhei <jonnarhei@gmail.com>
2026-01-19 14:29:45 +0100

    added new models for mealplan and recipe, and handling for zero values in env.go

--- internal/store/models/models.go
+++ internal/store/models/models.go
@@ -8,3 +8,16 @@ type User struct {
 	Password  string    `db:"password"`
 	CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
+
+type Mealplan struct {
+	MealplanID    int64     `db:"id" json:"id"`
+	UserID        int64     `db:"user_id" json:"user_id"`
+	WeekStartDate time.Time `db:"week_start_date" json:"week_start_date"`
+}
+
+type MealplanRecipe struct {
+	ID         int64 `db:"id" json:"id"`
+	MealplanID int64 `db:"mealplan_id" json:"mealplan_id"`
+	RecipeID   int64 `db:"recipe_id" json:"recipe_id"`
+	DayOfWeek  int16 `db:"day_of_week" json:"day_of_week"`
+}
