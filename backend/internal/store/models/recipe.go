99ef4b73a08655b6150d2a0a2732446db671b565
jonnarhei <jonnarhei@gmail.com>
2026-09-17 22:25:43 +0200

    recipe and ingredient model

---
+++ backend/internal/store/models/recipe.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+)
+
+type UserRecipe struct {
+	ID           int64                  `json:"id"`
+	UserID       int64                  `json:"user_id"`
+	Title        string                 `json:"title"`
+	Image        string                 `json:"image"`
+	SourceUrl    string                 `json:"source_url"`
+	Instructions string                 `json:"instructions"`
+	Servings     int                    `json:"servings"`
+	Ingredients  []UserRecipeIngredient `json:"ingredients"`
+	CreatedAt    time.Time              `json:"created_at"`
+	UpdatedAt    time.Time              `json:"updated_at"`
+}
+
+type UserRecipeIngredient struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Amount   int    `json:"amount"`
+	Unit     string `json:"unit"`
+	Position int    `json:"position"`
+}
